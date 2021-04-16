abcd6a92d3e389c018e7c8c4e8d330ced9174def
Daebeom Cho <dcho@c4r2s4.42seoul.kr>
2021-04-16 18:04:14 +0900

    cub3D

--- cub3d/includes/cub3d.h
+++ cub3d/includes/cub3d.h
@@ -6,12 +6,30 @@
 /*   By: dcho <dcho@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/14 18:06:04 by dcho              #+#    #+#             */
-/*   Updated: 2021/04/15 17:13:27 by dcho             ###   ########.fr       */
+/*   Updated: 2021/04/16 15:04:44 by dcho             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef CUB3D_H
+# define CUB3D_H
 
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+
+typedef struct		s_identifier
+{
+	unsigned int	r[2];
+	char			*no;
+	char			*so;
+	char			*we;
+	char			*ea;
+	char			*s;
+	unsigned int	f[3];
+	unsigned int	c[3];
+}					t_identifier;
+
+
+#endif
--- cub3d/srcs/main.c
+++ cub3d/srcs/main.c
@@ -1,34 +1,38 @@
 #include "mlx.h"
 #include "cub3d.h"
-#include "get_next_line.h"
+// #include "get_next_line.h"
 
 int		main(void)
 {
-	// void	*mlx;
-	// void	*win;
+	void	*mlx;
+	void	*win;
 
 	// if ((start = mlx_init()) == NULL);
 	// if ((windows = mlx_new_window(start, 900, 900, "Test")) == NULL);
 
-	char	*line;
-	int		ret;
-	int		fd;
+	// char	*line;
+	// int		ret;
+	// int		fd;
 
-	if ((fd = open("map.cub", O_RDONLY)) == -1)
-	{
-		perror("Error\n");
-		return (-1);
-	}
-	while ((ret = get_next_line(fd, &line)) > 0)
-	{
-		printf("%s\n", line);
-		free(line);
-	}
+	// if ((fd = open("map.cub", O_RDONLY)) == -1)
+	// {
+	// 	perror("Error\n");
+	// 	return (-1);
+	// }
+	// while ((ret = get_next_line(fd, &line)) > 0)
+	// {
+	// 	if (line[0] == 'R')
+	// 	{
+
+	// 	}
+	// 	printf("%s\n", line);
+	// 	free(line);
+	// }
 
 	// printf("%s\n",strerror(errno));
-	// mlx = mlx_init();
-	// win = mlx_new_window(mlx, 900, 900, "Test");
-	// mlx_loop(mlx);
+	mlx = mlx_init();
+	win = mlx_new_window(mlx, 900, 900, "Test");
+	mlx_loop(mlx);
 	return (0);
 }
 

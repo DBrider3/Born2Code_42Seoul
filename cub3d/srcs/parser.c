53d933a91b9c715480acc534f352fb0f0e7c1ba3
Daebeom Cho <dcho@c7r8s4.42seoul.kr>
2021-04-19 22:17:37 +0900

    cub3D

--- cub3d/srcs/parser.c
+++ cub3d/srcs/parser.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   parsing.c                                          :+:      :+:    :+:   */
+/*   parser.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dcho <dcho@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/17 22:12:38 by dcho              #+#    #+#             */
-/*   Updated: 2021/04/18 16:03:39 by dcho             ###   ########.fr       */
+/*   Updated: 2021/04/19 22:13:11 by dcho             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,48 @@
 #include "cub3d.h"
 #include "get_next_line.h"
 
-void	parsing(int fd, t_options *op)
+static int		parse_indentifier(char *line, t_options *op)
+{
+	char	**input;
+	char	*flag;
+
+	input  = ft_split(line, ' ');
+	flag = *input;
+	input++;
+	if (ft_strncmp(flag, "R", ft_strlen(flag)) == 0)
+	{
+		input_R();
+	}
+	else if (ft_strncmp(flag, "NO", ft_strlen(flag)) == 0)
+	{
+
+	}
+	else if (ft_strncmp(flag, "SO", ft_strlen(flag)) == 0);
+	else if (ft_strncmp(flag, "WE", ft_strlen(flag)) == 0);
+	else if (ft_strncmp(flag, "EA", ft_strlen(flag)) == 0);
+	else if (ft_strncmp(flag, "S", ft_strlen(flag)) == 0);
+	else if (ft_strncmp(flag, "F", ft_strlen(flag)) == 0);
+	else if (ft_strncmp(flag, "C", ft_strlen(flag)) == 0);
+	else;
+}
+
+
+void	parse_main(int fd, t_options *op)
 {
 	char	*line;
 	int		ret;
+	char 	*flag;
 	char	**input;
+	int		i;
 
 	while ((ret = get_next_line(fd, &line)) > 0)
 	{
-		if (line[0] == 'R')
-		{
-			input = ft_split(line, ' ');
-			while (*input[0])
-			{
+		input = ft_split(line, ' ');
+		flag = *input;
+		input++;
+		i = 0;
 
-				*input[0]++
-			}
-		}
+
 		printf("%s\n", line);
 		free(line);
 	}
--- cub3d/srcs/parser_input.c
+++ cub3d/srcs/parser_input.c
@@ -1,38 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cub3d.h                                            :+:      :+:    :+:   */
+/*   parser_input.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dcho <dcho@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/04/14 18:06:04 by dcho              #+#    #+#             */
-/*   Updated: 2021/04/19 20:50:20 by dcho             ###   ########.fr       */
+/*   Created: 2021/04/19 22:13:33 by dcho              #+#    #+#             */
+/*   Updated: 2021/04/19 22:16:33 by dcho             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CUB3D_H
-# define CUB3D_H
+#include "libft.h"
+#include "cub3d.h"
 
-#include <unistd.h>
-#include <fcntl.h>
-#include <stdio.h>
-#include <errno.h>
-#include <string.h>
-
-typedef struct		s_options
-{
-	unsigned int	r[2];
-	char			*no;
-	char			*so;
-	char			*we;
-	char			*ea;
-	char			*s;
-	unsigned int	f[3];
-	unsigned int	c[3];
-	char			**map;
-}					t_options;
-
-void				parse_main(int fd, t_options *op);
-
-
-#endif
+int		input_R()
